8872c6ada4b22ec0c5774d6401ff9060bd3cb74a
Gaëtan <gdvlsr@gmail.com>
2016-07-07 10:48:21 +0200

    C'est le merge final ! (presque)

---
+++ ClientLéger/WebApplicationBiblio/Default.aspx
@@ -0,0 +1,72 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplicationBiblio._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+
+
+    <div class="container-fluid">
+	    <div class="row">
+		    <div class="col-md-4">
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <asp:Label ID="LabelTitre" runat="server" Text="Titre : "></asp:Label>
+                            </td>
+                            <td>
+                                <asp:TextBox ID="TextBoxTitre" runat="server"></asp:TextBox>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <asp:Label ID="LabelAnneeParution" runat="server" Text="AnneeParution : "></asp:Label>
+                            </td>
+                            <td>
+                                <asp:TextBox ID="TextBoxAnneeParution" runat="server"></asp:TextBox>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <asp:Label ID="LabelAuteur" runat="server" Text="Auteur : "></asp:Label>
+                            </td>
+                            <td>
+                                <asp:DropDownList ID="DropDownListAuteur" runat="server" OnSelectedIndexChanged="DropDownListAuteur_SelectedIndexChanged"></asp:DropDownList>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <asp:Label ID="LabelGenre" runat="server" Text="Genre : "></asp:Label>
+                            </td>
+                            <td>
+                                <asp:DropDownList ID="DropDownListGenre" runat="server"></asp:DropDownList>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <asp:Label ID="LabelEmplacement" runat="server" Text="Emplacement : "></asp:Label>
+                            </td>
+                            <td>
+                                <asp:DropDownList ID="DropDownListEmplacement" runat="server"></asp:DropDownList>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colspan="2">
+                                <asp:Button ID="ButtonRechercher" runat="server" OnClick="ButtonRechercher_Click" Text="Rechercher" />
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                
+		    </div>
+		    <div class="col-md-8">
+                <asp:GridView ID="GridViewLivre" runat="server" AllowSorting="True" OnSorting="GridViewLivre_Sorting" ViewStateMode="Enabled" Width="100%" CssClass="Table"></asp:GridView>
+		    </div>
+		    <div class="col-md-12">
+                <asp:GridView ID="GridView1" runat="server" AllowSorting="True" OnSorting="GridViewLivre_Sorting" ViewStateMode="Enabled" Width="100%" CssClass="Table"></asp:GridView>
+		    </div>
+		    <div class="col-md-12">
+                <asp:GridView ID="GridView2" runat="server" AllowSorting="True" OnSorting="GridViewLivre_Sorting" ViewStateMode="Enabled" Width="100%"></asp:GridView>
+		    </div>
+	    </div>
+    </div>
+</asp:Content>
---
+++ ClientLéger/WebApplicationBiblio/Default.aspx.designer.cs
@@ -0,0 +1,141 @@
+﻿//------------------------------------------------------------------------------
+// <généré automatiquement>
+//     Ce code a été généré par un outil.
+//
+//     Les modifications apportées à ce fichier peuvent provoquer un comportement incorrect et seront perdues si
+//     le code est régénéré.
+// </généré automatiquement>
+//------------------------------------------------------------------------------
+
+namespace WebApplicationBiblio {
+    
+    
+    public partial class _Default {
+        
+        /// <summary>
+        /// Contrôle LabelTitre.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LabelTitre;
+
+        /// <summary>
+        /// Contrôle TextBoxTitre.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBoxTitre;
+
+        /// <summary>
+        /// Contrôle LabelAnneeParution.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LabelAnneeParution;
+
+        /// <summary>
+        /// Contrôle TextBoxAnneeParution.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBoxAnneeParution;
+
+        /// <summary>
+        /// Contrôle LabelAuteur.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LabelAuteur;
+
+        /// <summary>
+        /// Contrôle DropDownListAuteur.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownListAuteur;
+
+        /// <summary>
+        /// Contrôle LabelGenre.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LabelGenre;
+
+        /// <summary>
+        /// Contrôle DropDownListGenre.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownListGenre;
+
+        /// <summary>
+        /// Contrôle LabelEmplacement.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LabelEmplacement;
+
+        /// <summary>
+        /// Contrôle DropDownListEmplacement.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownListEmplacement;
+
+        /// <summary>
+        /// Contrôle ButtonRechercher.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button ButtonRechercher;
+
+        /// <summary>
+        /// Contrôle GridViewLivre.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridViewLivre;
+
+        /// <summary>
+        /// Contrôle GridView1.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridView1;
+
+        /// <summary>
+        /// Contrôle GridView2.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridView2;
+    }
+}
